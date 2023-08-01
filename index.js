e6b95610d03c085664dd891c299b46fa5133d806
Hebilicious <xsh4k3@gmail.com>
2023-08-02 04:13:49 +0700

    feat: add eslint config

---
+++ index.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: ["@antfu"],
+  rules: {
+    "@typescript-eslint/quotes": ["error", "double"],
+    "@typescript-eslint/comma-dangle": ["error", "never"],
+    "comma-dangle": ["error", "never"],
+    "yml/quotes": "off",
+    "antfu/if-newline": "off",
+    "n/prefer-global/process": ["error", "always"]
+  },
+  overrides: [
+    {
+      files: ["package.json"],
+      parser: "jsonc-eslint-parser",
+      rules: {
+        "jsonc/sort-keys": [
+          "error",
+          {
+            pathPattern: "^exports.*$",
+            order: ["require", "import", "types", "default"]
+          }
+        ]
+      }
+    }
+  ]
+}
